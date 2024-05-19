1aefa45b46e4848729c3a8f4502a4c79e6dc3b44
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-19 21:53:26 +0200

    feat(exercises/02_basic_calculator/10_as_casting/): completed

---
+++ 100-exercises-to-learn-rust/exercises/02_basic_calculator/10_as_casting/src/lib.rs
@@ -0,0 +1,25 @@
+// TODO: based on what you learned in this section, replace `todo!()` with
+//  the correct value after the conversion.
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn u16_to_u32() {
+        assert_eq!(47u16 as u32, 47u32);
+    }
+
+    #[test]
+    #[allow(overflowing_literals)]
+    fn u8_to_i8() {
+        // 255 is 
+        //       1  1  1  1  1  1  1  1
+        // 256 128 64 32 16  8  4  2  1
+        assert_eq!(255 as i8, -1i8);
+    }
+
+    #[test]
+    fn bool_to_u8() {
+        assert_eq!(true as u8, 1u8);
+    }
+}
