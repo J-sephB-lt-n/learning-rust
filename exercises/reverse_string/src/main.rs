893cc921f17cf046dbad799e1e4aef16aa364f8c
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-05-19 13:40:58 +0200

    feat(100-exercises-to-learn-rust/exercises/01_intro/01_syntax/src/lib.rs): completed exercise

--- exercises/reverse_string/src/main.rs
+++ exercises/reverse_string/src/main.rs
@@ -1,5 +1,11 @@
+use unicode_segmentation::UnicodeSegmentation;
+
 pub fn reverse(input: &str) -> String {
     // https://stackoverflow.com/questions/27996430/reversing-a-string-in-rust
     let drow: String = input.graphemes(true).rev().collect();
     return drow;
+}
+
+pub fn main() {
+    reverse("sinep")
 }
