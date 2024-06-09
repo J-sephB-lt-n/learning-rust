944d5a31c135fe60954dab8d4faf7ea9a2d7bef8
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2024-06-09 22:34:33 +0200

    feat(03_ticket_v1/12_outro/): completed

---
+++ 100-exercises-to-learn-rust/exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -0,0 +1,71 @@
+// TODO: Define a new `Order` type.
+//   It should keep track of three pieces of information: `product_name`, `quantity`, and `unit_price`.
+//   The product name can't be empty and it can't be longer than 300 characters.
+//   The quantity must be strictly greater than zero.
+//   The unit price is in cents and must be strictly greater than zero.
+//   Order must include a method named `total` that returns the total price of the order.
+//   Order must provide setters and getters for each field.
+//
+// Tests are located in a different place this time—in the `tests` folder.
+// The `tests` folder is a special location for `cargo`. It's where it looks for **integration tests**.
+// Integration here has a very specific meaning: they test **the public API** of your project.
+// You'll need to pay attention to the visibility of your types and methods; integration
+// tests can't access private or `pub(crate)` items.
+
+pub struct Order {
+    product_name: String,
+    quantity: u16,
+    unit_price: u16,
+}
+
+impl Order {
+    fn validate_product_name(product_name: &String) {
+        if product_name.is_empty() || product_name.len() > 300 {
+            panic!("product name is invalid");
+        }
+    }
+    fn validate_quantity(quantity: &u16) {
+        if quantity <= &0 {
+            panic!("invalid quantity");
+        }
+    }
+    fn validate_unit_price(unit_price: &u16) {
+        if unit_price <= &0 {
+            panic!("invalid unit price");
+        }
+    }
+    pub fn new(product_name: String, quantity: u16, unit_price: u16) -> Order {
+        Self::validate_product_name(&product_name);
+        Self::validate_quantity(&quantity);
+        Self::validate_unit_price(&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+    pub fn total(&self) -> u16 {
+        &self.quantity * &self.unit_price
+    }
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        Self::validate_product_name(&new_product_name);
+        self.product_name = new_product_name;
+    }
+    pub fn set_quantity(&mut self, new_quantity: u16) {
+        Self::validate_quantity(&new_quantity);
+        self.quantity = new_quantity;
+    }
+    pub fn set_unit_price(&mut self, new_unit_price: u16) {
+        Self::validate_unit_price(&new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+    pub fn quantity(&self) -> &u16 {
+        &self.quantity
+    }
+    pub fn unit_price(&self) -> &u16 {
+        &self.unit_price
+    }
+}
